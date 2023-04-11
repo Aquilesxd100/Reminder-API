ff63f0a75d10d0d34a23712433a1b57a1dafe063
Aquilesxd <fernandoalanfillmann@gmail.com>
2023-04-11 20:21:41 -0300

    melhorias no middleware de login

--- src/middlewares/loginAuthMiddleware.ts
+++ src/middlewares/loginAuthMiddleware.ts
@@ -5,7 +5,10 @@ import User from "../models/User";
 export default function loginAuthMiddleware
 (req : any, res : Response, next : NextFunction) {
     const { userName, password } = req.params;
-    const currentUser : User | undefined = usersList.getUserByUserName(userName);
+    const currentUser : User | undefined = typeof userName === "string"
+    ? usersList.getUserByUserName(userName)
+    : undefined;
+    
     if(!currentUser || currentUser.getPassword() !== password) {
         return res.status(400).send({ message: "Login ou senha incorreto." });
     };

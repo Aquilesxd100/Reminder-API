5d297ff754248998afe963decb20b13dec981c25
Aquilesxd <fernandoalanfillmann@gmail.com>
2023-06-21 10:44:51 -0300

    atualizacao de arquivo env

--- src/app/envs/env.ts
+++ src/app/envs/env.ts
@@ -1,6 +1,7 @@
 import "dotenv/config";
 export const apiEnv = {
     dbUrl: process.env.DATABASE_URL,
+    testDbUrl: process.env.TEST_DATABASE_URL,
     port: process.env.PORT
 };
 export const authEnv = process.env.ACCESS_TOKEN_SECRET as string;

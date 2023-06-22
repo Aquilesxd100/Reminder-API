7b43fabde4844049c88eab887d6ed89f607a9651
Aquilesxd <fernandoalanfillmann@gmail.com>
2023-06-22 11:27:19 -0300

    adicao de teste unit para useCase de arquivamento

--- tests/app/features/reminder/usecases/changeArchiveStatus.spec.ts
+++ tests/app/features/reminder/usecases/changeArchiveStatus.spec.ts
@@ -2,14 +2,26 @@ import changeArchiveStatus from "../../../../../src/app/features/reminder/usecas
 import { RemindersTypeOrmRepository } from "../../../../../src/app/features/reminder/repository/remindersTypeOrmRepository"
 import { RemindersEntity } from "../../../../../src/app/shared/entities/remindersEntity";
 
-describe('Deve retornar uma string confirmando o arquivamento do recado.', () => {
+describe('Testes do useCase changeArchiveStatus.', () => {
     beforeAll(() => {
         jest.spyOn(RemindersTypeOrmRepository.prototype, "saveReminder").mockResolvedValue()
 
     });
     afterAll(() => jest.clearAllMocks());
-    test("Deve retornar string", async () => {
-        const result = await changeArchiveStatus(new RemindersEntity);
+
+    const testReminder : RemindersEntity = new RemindersEntity;
+
+    test("Deve retornar uma string confirmando o arquivamento do recado.", async () => {
+        testReminder.archived = false;
+
+        const result = await changeArchiveStatus(testReminder);
         expect(result).toEqual("Recado arquivado com sucesso!");
+    });
+
+    test("Deve retornar uma string confirmando o desarquivamento do recado.", async () => {
+        testReminder.archived = true;
+
+        const result = await changeArchiveStatus(testReminder);
+        expect(result).toEqual("Recado desarquivado com sucesso!");
     });
 });
